1e47bd9fbede75c95f58427d48b636ff2d081f65
SEGUNDO R NOVOA <rafael.novoa@gmail.com>
2018-01-12 02:55:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var height = "74"
-var name = "Joe"
+var name1 = "Joe"
 
 console.log("Name:")
-console.log(name)
+console.log(name1)
 
 console.log("Height:")
 console.log(height)
